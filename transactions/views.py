f90363cde073a42788d1017587772cf42a503858
dg-codes1 <douglasclaudino10.dc@gmail.com>
2025-04-02 08:35:57 -0300

    feat: add get balance from user endpoint

--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import User
@@ -12,3 +14,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def balance(self, request, pk=None):
+        user = self.get_object()
+        return Response({'balance': user.wallet_balance}, status=status.HTTP_200_OK)
