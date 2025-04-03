174656f98610be8d67bc2f4cc64da28155907727
dg-codes1 <douglasclaudino10.dc@gmail.com>
2025-04-02 21:33:23 -0300

    feat: list transactions

--- transactions/serializers/transfer.py
+++ transactions/serializers/transfer.py
@@ -5,4 +5,12 @@ from transactions.models import Transfer
 class TransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
-        fields = ('payer', 'receiver', 'value')
+        fields = "__all__"
+
+class ListTransferenciaSerializer(serializers.ModelSerializer):
+    payer_username = serializers.CharField(source='payer.username', read_only=True)
+    receiver_username = serializers.CharField(source='receiver.username', read_only=True)
+
+    class Meta:
+        model = Transfer
+        fields = ('uuid', 'payer_username', 'receiver_username', 'value', 'date_time')
--- transactions/views/transfer.py
+++ transactions/views/transfer.py
@@ -1,14 +1,23 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from transactions.models import Transfer
-from transactions.serializers import TransferSerializer
+from transactions.models import Transfer, User
+from transactions.serializers import TransferSerializer, ListTransferenciaSerializer
 
 
 class TransferViewSet(viewsets.ModelViewSet):
     queryset = Transfer.objects.all()
-    serializer_class = TransferSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ListTransferenciaSerializer
+        return TransferSerializer
+
+    def get_queryset(self):
+        if self.action in ['create', 'retrieve', 'list']:
+            return User.objects.filter(pk=self.request.user.pk)
+        return super().get_queryset()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -33,3 +42,17 @@ class TransferViewSet(viewsets.ModelViewSet):
         receiver.save()
 
         return Response({'message': 'Transfer was successful'}, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        user = request.user
+        transfers = Transfer.objects.filter(payer=user).order_by('-date_time')
+
+        from_date = request.query_params.get('from_date')
+        to_date = request.query_params.get('to_date')
+
+        if from_date and to_date:
+            transfers = transfers.filter(
+                date_time__date__range=[from_date, to_date])
+
+        serializer = self.get_serializer(transfers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
